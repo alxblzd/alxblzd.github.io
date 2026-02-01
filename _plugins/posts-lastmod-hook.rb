b7a4dbdfb0755187f548291963db8db060f9b173
Alxblzd <69093161+alxblzd@users.noreply.github.com>
2026-02-01 15:56:15 +0100

    updated image

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -9,7 +9,7 @@ Jekyll::Hooks.register :posts, :post_init do |post|
   next unless git_repo
 
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
-  return unless commit_num.to_i > 1
+  next unless commit_num.to_i > 1
 
   lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
   post.data['last_modified_at'] = lastmod_date
